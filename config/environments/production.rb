acde19c66bcec8dfb1777f18fb352c905bcc61a3
arnaudlothe <arnaud.lothe@gmail.com>
2025-02-13 15:49:12 +0100

    Remove solid_cache and fix database.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
   config.silence_healthcheck_path = "/up"
   config.active_support.report_deprecations = false
-  config.cache_store = :solid_cache_store
+  # config.cache_store = :solid_cache_store
   # config.active_job.queue_adapter = :solid_queue
   # config.solid_queue.connects_to = { database: { writing: :queue } }
   config.i18n.fallbacks = true
