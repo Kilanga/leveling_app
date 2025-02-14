6561f36e154c560e66fc560a376c094967153561
Kilanga <34622052+Kilanga@users.noreply.github.com>
2025-02-14 10:21:42 +0000

    Ajout d'un chart pour les niveaux

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,5 @@
 pin "application"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "chart.js" # @4.4.7
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
