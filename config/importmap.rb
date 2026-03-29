0f8fdd5fcf08b04588ba659662aab8e48c55a6a2
root <arnaud.lothe@gmail.com>
2026-03-29 10:22:04 +0000

    Pin shop preview controller in importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,4 @@ pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/boots
 pin "@popperjs/core", to: "@popperjs--core.js"
 pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.js"
 pin "controllers/stats_chart", to: "controllers/stats_chart.js"
+pin "controllers/shop_preview", to: "controllers/shop_preview.js"
