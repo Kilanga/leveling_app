7c54f1ff5d076e0b2950be6b0e797ed9fd532c23
Arnaud <arnaud.lothe@gmail.com>
2026-07-04 16:20:37 +0200

    fix(test): assertion insensible à la casse (titre boutique uppercasé par CSS)

--- test/system/purchase_test.rb
+++ test/system/purchase_test.rb
@@ -9,7 +9,8 @@ class PurchaseTest < ApplicationSystemTestCase
   test "l'utilisateur voit la boutique et ses soldes" do
     visit new_purchase_path
 
-    assert_text I18n.t("purchases.new.page_title")
+    # Le titre est affiché en majuscules par le CSS -> comparaison insensible à la casse
+    assert_text(/#{Regexp.escape(I18n.t("purchases.new.page_title"))}/i)
     assert_text "Orbes"
     assert_text "Fragments"
   end
